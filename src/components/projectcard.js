502b7ed13b8f034513b4bcdbe8a4134bc1095440
ashutosh-b-b <a.bharambe123@gmail.com>
2024-08-07 00:49:04 +0530

    revamp projects page

---
+++ src/components/projectcard.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+import "../styles/projects.scss"
+
+export default function ProjectCard(props) {
+    return(
+        <div className = "card" id = "card-1">
+            <div class="blob-btn">
+            <div className = "card-head"><div>{props.head}</div></div>
+          <div className = "card-body">{props.body}</div>
+
+              <span class="blob-btn__inner">
+                <span class="blob-btn__blobs">
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+                  <span class="blob-btn__blob"></span>
+
+                </span>
+              </span>
+            </div>
+            <br/>
+
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+                <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+              </filter>
+            </defs>
+          </svg>
+        </div>
+    )
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,41 +1,24 @@
 import * as React from 'react';
 
-import "../styles/projects.css"
-
+import "../styles/projects.scss"
+import ProjectCard from './projectcard';
 export default function ProjectBar() {
   return (
     <div className = "project-main">
       <div className = "project-head">My Work</div>
       <div className = "projects-container">
-        <div className = "card" id = "card-1">
-          <div className = "card-head"><div>SciML: HighDimPDE.jl</div></div>
-          <div className = "card-body">HighDimPDE is an open source Julia package for solving high dimensional PDEs using deep learning</div>
-          {/* <div className = "card-action">
-            <div className='card-btn'>
-              <a>View More</a>
-            </div>
-          </div> */}
-        </div>
-
-        <div className = "card" id = "card-2">
-          <div className = "card-head"><div>NLP for Hinglish</div></div>
-          <div className = "card-body">Implemented deep learning algorithms and pipeline for NLP tasks on Hindi English mixed corpus.</div>
-          {/* <div className = "card-action">
-            <div className='card-btn'>
-            <a>View More</a>
-            </div>
-          </div> */}
-        </div>
-        <div className = "card" id = "card-3">
-          <div className = "card-head"><div>Quizio</div></div>
-          <div className = "card-body">Quizio is an online platform for conducting exams and recruitments in IITR Campus</div>
-          
-          {/* <div className = "card-action">
-            <div className='card-btn'>
-            <a>View More</a>
-            </div>
-          </div> */}
-        </div>
+      <ProjectCard 
+          head = "SciML: HighDimPDE.jl"
+          body = "HighDimPDE is an open source Julia package for solving high dimensional PDEs using deep learning"
+        />
+        <ProjectCard 
+          head = "NLP for Hinglish"
+          body = "Implemented deep learning algorithms and pipeline for NLP tasks on Hindi English mixed corpus."
+        />
+        <ProjectCard 
+          head = "Quizio"
+          body = "Quizio is an online platform for conducting exams and recruitments in IITR Campus"
+        />
        
       </div>
     </div>
